65dd9bd8b95b83bc104d501e732dce8627dbd4e5
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-05-27 11:37:29 -0600

    Update blended.sql
    ga4_purchases, ga4_revenue

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -4,6 +4,7 @@
 
 WITH data as
     (SELECT 
+        channel,
         date::date, 
         date_granularity, 
         COALESCE(SUM(spend),0) as spend, 
@@ -15,22 +16,44 @@ WITH data as
         COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(add_to_cart),0) as add_to_cart,
         COALESCE(SUM(purchases),0) as purchases, 
-        COALESCE(SUM(revenue),0) as revenue
+        COALESCE(SUM(revenue),0) as revenue,
+        COALESCE(SUM(ga4_purchases),0) as ga4_purchases,
+        COALESCE(SUM(ga4_revenue),0) as ga4_revenue
     FROM
-        (SELECT date, date_granularity, spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, link_clicks as clicks, add_to_cart, purchases, revenue
+        (SELECT 'Meta' as channel, date, date_granularity, 
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, link_clicks as clicks, add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue
         FROM {{ source('reporting','facebook_ad_performance') }}
-        WHERE campaign_name !~* 'traffic' OR campaign_name != '[Superbolt] - Brand Awareness - March 2022'
+        --WHERE campaign_name !~* 'traffic' OR campaign_name != '[Superbolt] - Brand Awareness - March 2022'
         UNION ALL
-        SELECT date, date_granularity, spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, add_to_cart, purchases, revenue
+        SELECT 'Google Ads' as channel, date, date_granularity, 
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
         UNION ALL
-        SELECT date, date_granularity, spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, 0 as add_to_cart, purchases, revenue
+        SELECT 'Bing' as channel, date, date_granularity, 
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, 0 as add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue
         FROM {{ source('reporting','bingads_campaign_performance') }}
         UNION ALL
-        SELECT date, date_granularity, sum(0) as spend, sum(gross_sales) as gross_sales, sum(subtotal_sales) as subtotal_sales, sum(orders) as orders, sum(first_orders) as first_orders, 0 as impressions, 0 as clicks, 0 as add_to_cart, 0 as purchases, 0 as revenue
+        SELECT 'Shopify' as channel, date, date_granularity, 
+            sum(0) as spend, sum(gross_sales) as gross_sales, sum(subtotal_sales) as subtotal_sales, sum(orders) as orders, sum(first_orders) as first_orders, 0 as impressions, 
+            0 as clicks, 0 as add_to_cart, 0 as purchases, 0 as revenue, sum(0) as ga4_purchases, sum(0) as ga4_revenue
         FROM {{ source('reporting','shopify_sales') }}
-        GROUP BY 1,2)
-    GROUP BY date, date_granularity)
+        GROUP BY 1,2,3
+        UNION ALL
+        SELECT 'Meta' as channel, date, date_granularity, 
+            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
+            sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
+        FROM {{ source('reporting','ga4_campaign_performance') }}
+        WHERE source_medium = 'facebook / paid'
+        GROUP BY 1,2,3
+        UNION ALL
+        SELECT 'Google Ads' as channel, date, date_granularity, 
+            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
+            sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
+        FROM {{ source('reporting','ga4_campaign_performance') }}
+        WHERE source_medium = 'google / cpc'
+        GROUP BY 1,2,3
+        )
+    GROUP BY channel, date, date_granularity)
     
 SELECT * FROM
 {{ source('utilities','dates') }}
