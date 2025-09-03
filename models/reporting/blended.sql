1e26342d0410d3b92f8b9995a6337f2e33ceca5c
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-09-03 12:42:08 -0600

    Update blended.sql
    sessions

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -18,37 +18,38 @@ WITH data as
         COALESCE(SUM(purchases),0) as purchases, 
         COALESCE(SUM(revenue),0) as revenue,
         COALESCE(SUM(ga4_purchases),0) as ga4_purchases,
-        COALESCE(SUM(ga4_revenue),0) as ga4_revenue
+        COALESCE(SUM(ga4_revenue),0) as ga4_revenue,
+        COALESCE(SUM(sessions),0) as sessions
     FROM
         (SELECT 'Meta' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, link_clicks as clicks, add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, link_clicks as clicks, add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue, sum(0) as sessions
         FROM {{ source('reporting','facebook_ad_performance') }}
         --WHERE campaign_name !~* 'traffic' OR campaign_name != '[Superbolt] - Brand Awareness - March 2022'
         UNION ALL
         SELECT 'Google Ads' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue, sum(0) as sessions
         FROM {{ source('reporting','googleads_campaign_performance') }}
         UNION ALL
         SELECT 'Bing' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, 0 as add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, 0 as add_to_cart, purchases, revenue, 0 as ga4_purchases, 0 as ga4_revenue, sum(0) as sessions
         FROM {{ source('reporting','bingads_campaign_performance') }}
         UNION ALL
         SELECT 'Shopify' as channel, date, date_granularity, 
             sum(0) as spend, sum(gross_sales) as gross_sales, sum(subtotal_sales) as subtotal_sales, sum(orders) as orders, sum(first_orders) as first_orders, 0 as impressions, 
-            0 as clicks, 0 as add_to_cart, 0 as purchases, 0 as revenue, sum(0) as ga4_purchases, sum(0) as ga4_revenue
+            0 as clicks, 0 as add_to_cart, 0 as purchases, 0 as revenue, sum(0) as ga4_purchases, sum(0) as ga4_revenue, sum(0) as sessions
         FROM {{ source('reporting','shopify_sales') }}
         GROUP BY 1,2,3
         UNION ALL
         SELECT 'Meta' as channel, date, date_granularity, 
             sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
-            sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
+            sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue, sum(sessions) as sessions
         FROM {{ source('reporting','ga4_campaign_performance') }}
         WHERE source_medium = 'facebook / paid'
         GROUP BY 1,2,3
         UNION ALL
         SELECT 'Google Ads' as channel, date, date_granularity, 
             sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
-            sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
+            sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue, sum(sessions) as sessions
         FROM {{ source('reporting','ga4_campaign_performance') }}
         WHERE source_medium = 'google / cpc'
         GROUP BY 1,2,3
