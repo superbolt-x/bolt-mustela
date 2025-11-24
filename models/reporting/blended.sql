adb0ef8edf3dd9e2c6e81f1826168667eee26456
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-11-24 21:05:16 +0100

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -33,6 +33,10 @@ WITH data as
             spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, add_to_cart, purchases, revenue, 
 			0 as sessions, 0 as engaged_sessions, 0 as new_users, 0 as ga4_purchases, 0 as ga4_revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
+		SELECT 'Tiktok Ads' as channel, date, date_granularity, 
+            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, atc as add_to_cart, purchases, revenue, 
+			0 as sessions, 0 as engaged_sessions, 0 as new_users, 0 as ga4_purchases, 0 as ga4_revenue
+        FROM {{ source('reporting','tiktok_campaign_performance') }}
         UNION ALL
         SELECT 'Bing' as channel, date, date_granularity, 
             spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, 0 as add_to_cart, purchases, revenue, 
@@ -60,11 +64,18 @@ WITH data as
         WHERE source_medium = 'google / cpc'
         GROUP BY 1,2,3
         UNION ALL
+		SELECT 'Tiktok Ads' as channel, date, date_granularity, 
+            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
+            sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(new_users) as new_users, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
+        FROM {{ source('reporting','ga4_campaign_performance') }}
+        WHERE source_medium = 'paid_tiktok / cpm'
+        GROUP BY 1,2,3
+        UNION ALL
         SELECT 'Other' as channel, date, date_granularity, 
             sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
             sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(new_users) as new_users, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
         FROM {{ source('reporting','ga4_campaign_performance') }}
-        WHERE source_medium NOT IN ('facebook / paid','google / cpc') OR source_medium IS NULL
+        WHERE source_medium NOT IN ('facebook / paid','google / cpc','paid_tiktok / cpm') OR source_medium IS NULL
         GROUP BY 1,2,3
         )
     GROUP BY channel, date, date_granularity)
