7355518bbfe560ef748b8ebc401b757f48093311
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-11-26 08:40:29 +0100

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -2,86 +2,141 @@
     alias = target.database + '_blended'
 )}}
 
-WITH data as
-    (SELECT 
-        channel,
-        date::date, 
-        date_granularity, 
-        COALESCE(SUM(spend),0) as spend, 
-        COALESCE(SUM(gross_sales),0) as gross_sales, 
-        COALESCE(SUM(subtotal_sales),0) as subtotal_sales,
-        COALESCE(SUM(orders),0) as orders,
-        COALESCE(SUM(first_orders),0) as first_orders,
-        COALESCE(SUM(impressions),0) as impressions, 
-        COALESCE(SUM(clicks),0) as clicks, 
-        COALESCE(SUM(add_to_cart),0) as add_to_cart,
-        COALESCE(SUM(purchases),0) as purchases, 
-        COALESCE(SUM(revenue),0) as revenue,
-		COALESCE(SUM(sessions),0) as sessions,
-		COALESCE(SUM(engaged_sessions),0) as engaged_sessions,
-		COALESCE(SUM(new_users),0) as new_users,
-        COALESCE(SUM(ga4_purchases),0) as ga4_purchases,
-        COALESCE(SUM(ga4_revenue),0) as ga4_revenue
+{{ config (
+    alias = target.database + '_blended_performance'
+)}}
+
+{%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
+
+
+WITH refund_order_data AS
+    (SELECT date, day, week, month, quarter, year, 
+        order_id, customer_order_index, gross_revenue, total_revenue, subtotal_discount, 0 as subtotal_refund 
+    FROM {{ source('reporting','shopify_daily_sales_by_order') }}
+    WHERE cancelled_at IS NULL
+    UNION ALL
+    SELECT date, day, week, month, quarter, year, 
+        null as order_id, null as customer_order_index, 0 as gross_revenue, 0 as total_revenue, 0 as subtotal_discount, subtotal_refund 
+    FROM {{ source('reporting','shopify_daily_refunds') }} 
+    WHERE cancelled_at IS NULL),
+    
+    initial_sho_data AS (
+        {% for granularity in date_granularity_list %}
+        SELECT 
+            '{{granularity}}' as date_granularity,
+            {{granularity}} as date,
+            COUNT(DISTINCT order_id) as shopify_orders, 
+            COUNT(DISTINCT CASE WHEN customer_order_index = 1 THEN order_id END) as shopify_first_orders,
+            SUM(COALESCE(gross_revenue,0)-COALESCE(subtotal_discount,0)) as shopify_sales,
+            SUM(CASE WHEN customer_order_index = 1 THEN gross_revenue-subtotal_discount END) as shopify_first_sales,
+            COALESCE(SUM(subtotal_refund),0) as shopify_refund,
+            SUM(CASE WHEN customer_order_index = 1 THEN subtotal_refund END) as shopify_first_refund
+        FROM refund_order_data
+        GROUP BY date_granularity, {{granularity}}
+        {% if not loop.last %}UNION ALL{% endif %}
+        {% endfor %}
+    ),
+    
+paid_data as
+    (SELECT channel, campaign_id::varchar as campaign_id, campaign_name, date::date, date_granularity, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, 
+        COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue, 
+        0 as shopify_first_orders, 0 as shopify_orders, 0 as shopify_first_sales, 0 as shopify_sales, 0 as shopify_first_net_sales, 0 as shopify_net_sales
     FROM
-        (SELECT 'Meta' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, link_clicks as clicks, add_to_cart, purchases, revenue, 
-			0 as sessions, 0 as engaged_sessions, 0 as new_users, 0 as ga4_purchases, 0 as ga4_revenue
+        (SELECT 'Meta' as channel, campaign_id, campaign_name, date, date_granularity, 
+            spend, link_clicks as clicks, impressions, add_to_cart, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','facebook_ad_performance') }}
-        --WHERE campaign_name !~* 'traffic' OR campaign_name != '[Superbolt] - Brand Awareness - March 2022'
         UNION ALL
-        SELECT 'Google Ads' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, add_to_cart, purchases, revenue, 
-			0 as sessions, 0 as engaged_sessions, 0 as new_users, 0 as ga4_purchases, 0 as ga4_revenue
+        SELECT 'Google Ads' as channel, campaign_id, campaign_name, date, date_granularity,
+            spend, clicks, impressions, add_to_cart, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','googleads_campaign_performance') }}
 		UNION ALL
-		SELECT 'Tiktok Ads' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, atc as add_to_cart, purchases, revenue, 
-			0 as sessions, 0 as engaged_sessions, 0 as new_users, 0 as ga4_purchases, 0 as ga4_revenue
+		SELECT 'Tiktok Ads' as channel, campaign_id, campaign_name, date, date_granularity, 
+			spend, clicks, impressions, atc as add_to_cart, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','tiktok_campaign_performance') }}
         UNION ALL
-        SELECT 'Bing' as channel, date, date_granularity, 
-            spend, 0 as gross_sales, 0 as subtotal_sales, 0 as orders, 0 as first_orders, impressions, clicks, 0 as add_to_cart, purchases, revenue, 
-			0 as sessions, 0 as engaged_sessions, 0 as new_users, 0 as ga4_purchases, 0 as ga4_revenue
+        SELECT 'Bing' as channel, campaign_id, campaign_name, date, date_granularity, 
+            spend, clicks, impressions, 0 as add_to_cart, purchases as paid_purchases, revenue as paid_revenue
         FROM {{ source('reporting','bingads_campaign_performance') }}
-        UNION ALL
-        SELECT 'Shopify' as channel, date, date_granularity, 
-            sum(0) as spend, sum(gross_sales) as gross_sales, sum(subtotal_sales) as subtotal_sales, sum(orders) as orders, sum(first_orders) as first_orders, 0 as impressions, 
-            0 as clicks, 0 as add_to_cart, 0 as purchases, 0 as revenue, 
-			sum(0) as sessions, sum(0) as engaged_sessions, sum(0) as new_users, sum(0) as ga4_purchases, sum(0) as ga4_revenue
-        FROM {{ source('reporting','shopify_sales') }}
-        GROUP BY 1,2,3
-        UNION ALL
-        SELECT 'Meta' as channel, date, date_granularity, 
-            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
-            sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(new_users) as new_users, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
-        FROM {{ source('reporting','ga4_campaign_performance') }}
-        WHERE source_medium = 'facebook / paid'
-        GROUP BY 1,2,3
-        UNION ALL
-        SELECT 'Google Ads' as channel, date, date_granularity, 
-            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
-            sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(new_users) as new_users, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
-        FROM {{ source('reporting','ga4_campaign_performance') }}
-        WHERE source_medium = 'google / cpc'
-        GROUP BY 1,2,3
-        UNION ALL
-		SELECT 'Tiktok Ads' as channel, date, date_granularity, 
-            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
-            sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(new_users) as new_users, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
-        FROM {{ source('reporting','ga4_campaign_performance') }}
-        WHERE source_medium = 'paid_tiktok / cpm'
-        GROUP BY 1,2,3
-        UNION ALL
-        SELECT 'Other' as channel, date, date_granularity, 
-            sum(0) as spend, sum(0) as gross_sales, sum(0) as subtotal_sales, sum(0) as orders, sum(0) as first_orders, sum(0) as impressions, sum(0) as clicks, sum(0) as add_to_cart, sum(0) as purchases, sum(0) as revenue,
-            sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(new_users) as new_users, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
-        FROM {{ source('reporting','ga4_campaign_performance') }}
-        WHERE source_medium NOT IN ('facebook / paid','google / cpc','paid_tiktok / cpm') OR source_medium IS NULL
-        GROUP BY 1,2,3
         )
-    GROUP BY channel, date, date_granularity)
+    GROUP BY channel, campaign_id, campaign_name, date, date_granularity),
+
+ga4_data as 
+    (SELECT session_campaign_id::varchar as campaign_id, date, date_granularity, 
+    sum(sessions) as sessions, sum(engaged_sessions) as engaged_sessions, sum(purchase) as ga4_purchases, sum(purchase_value) as ga4_revenue
+    FROM {{ source('reporting','ga4_campaign_performance') }}
+    GROUP BY 1,2,3),
+
+paid_ga4_data as (
+  SELECT 
+    case when campaign_name is null then 'Not Paid' else channel end as channel, campaign_name, date::date, date_granularity,
+    SUM(COALESCE(spend, 0)) AS spend,
+    SUM(COALESCE(clicks, 0)) AS clicks,
+    SUM(COALESCE(impressions, 0)) AS impressions,
+	SUM(COALESCE(add_to_cart, 0)) AS add_to_cart,
+    SUM(COALESCE(paid_purchases, 0)) AS paid_purchases,
+    SUM(COALESCE(paid_revenue, 0)) AS paid_revenue,
+    SUM(COALESCE(shopify_first_orders, 0)) AS shopify_first_orders,
+    SUM(COALESCE(shopify_orders, 0)) AS shopify_orders,
+    SUM(COALESCE(shopify_first_sales, 0)) AS shopify_first_sales,
+    SUM(COALESCE(shopify_sales, 0)) AS shopify_sales,
+    SUM(COALESCE(shopify_first_net_sales, 0)) AS shopify_first_net_sales,
+    SUM(COALESCE(shopify_net_sales, 0)) AS shopify_net_sales,
+    SUM(COALESCE(sessions, 0)) AS sessions,
+    SUM(COALESCE(engaged_sessions, 0)) AS engaged_sessions,
+    SUM(COALESCE(ga4_purchases, 0)) AS ga4_purchases,
+    SUM(COALESCE(ga4_revenue, 0)) AS ga4_revenue
+  FROM paid_data FULL OUTER JOIN ga4_data USING(date,date_granularity,campaign_id)
+  GROUP BY 1,2,3,4),
+
+sho_data as
+    (SELECT
+            'Shopify' as channel,
+            '(not set)' as campaign_name,
+            date,
+            date_granularity,
+            0 as spend,
+            0 as clicks,
+            0 as impressions,
+			0 as add_to_cart,
+            0 as paid_purchases,
+            0 as paid_revenue, 
+            COALESCE(SUM(shopify_first_orders),0) as shopify_first_orders, 
+            COALESCE(SUM(shopify_orders),0) as shopify_orders, 
+            COALESCE(SUM(shopify_first_sales),0) as shopify_first_sales, 
+            COALESCE(SUM(shopify_sales), 0) as shopify_sales,
+            COALESCE(SUM(shopify_first_sales),0)-COALESCE(SUM(shopify_first_refund),0) as shopify_first_net_sales,
+            COALESCE(SUM(shopify_sales),0)-COALESCE(SUM(shopify_refund),0) as shopify_net_sales,
+            0 as sessions,
+            0 as engaged_sessions,
+            0 as ga4_purchases,
+            0 as ga4_revenue 
+        FROM initial_sho_data 
+        GROUP BY channel, date, campaign_name, date_granularity
+    )
     
-SELECT * FROM
-{{ source('utilities','dates') }}
-LEFT JOIN data USING (date)
-WHERE date <= current_date
+SELECT 
+    channel,
+    campaign_name,
+    date,
+    date_granularity,
+    spend,
+    clicks,
+    impressions,
+	add_to_cart,
+    paid_purchases,
+    paid_revenue,
+    shopify_first_orders,
+    shopify_orders,
+    shopify_first_sales,
+    shopify_sales,
+    shopify_first_net_sales,
+    shopify_net_sales,
+    sessions,
+    engaged_sessions,
+    ga4_purchases,
+    ga4_revenue 
+FROM (
+    SELECT * FROM paid_ga4_data
+    UNION ALL 
+    SELECT * FROM sho_data
+)
