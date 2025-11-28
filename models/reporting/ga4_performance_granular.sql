6c6c988196fcb84c292a4606104a21ce585ddb22
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-11-28 07:58:13 +0100

    Add GA4 performance granular SQL model

---
+++ models/reporting/ga4_performance_granular.sql
@@ -0,0 +1,25 @@
+{{ config (
+    alias = target.database + '_ga4_performance_granular'
+)}}
+
+SELECT 
+profile,
+source_medium,
+campaign_name,
+campaign_id,
+adset,
+ad,
+landing_page,
+date,
+date_granularity,
+new_users,
+sessions,
+average_session_duration,
+engaged_sessions,
+bounce_rate,
+purchase_value,
+total_revenue,
+purchase,
+session_duration,
+bounced_sessions
+FROM {{ ref('ga4_performance_by_campaign') }}
