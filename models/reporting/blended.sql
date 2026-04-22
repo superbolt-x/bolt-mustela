5564f6b491b538547fd66a5cbb4d9b036ba0fe46
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-22 15:28:03 -0400

    Refactor SQL for blended reporting data

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -2,35 +2,7 @@
     alias = target.database + '_blended'
 )}}
 
-{%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
-
-
-WITH refund_order_data AS
-    (SELECT date, day, week, month, quarter, year, 
-        order_id, customer_order_index, gross_revenue, total_revenue, subtotal_discount, 0 as subtotal_refund 
-    FROM {{ source('reporting','shopify_daily_sales_by_order') }}
-    UNION ALL
-    SELECT date, day, week, month, quarter, year, 
-        null as order_id, null as customer_order_index, 0 as gross_revenue, 0 as total_revenue, 0 as subtotal_discount, subtotal_refund 
-    FROM {{ source('reporting','shopify_daily_refunds') }} ),
-    
-    initial_sho_data AS (
-        {% for granularity in date_granularity_list %}
-        SELECT 
-            '{{granularity}}' as date_granularity,
-            {{granularity}} as date,
-            COUNT(DISTINCT order_id) as shopify_orders, 
-            COUNT(DISTINCT CASE WHEN customer_order_index = 1 THEN order_id END) as shopify_first_orders,
-            SUM(COALESCE(gross_revenue,0)-COALESCE(subtotal_discount,0)) as shopify_sales,
-            SUM(CASE WHEN customer_order_index = 1 THEN gross_revenue-subtotal_discount END) as shopify_first_sales,
-            COALESCE(SUM(subtotal_refund),0) as shopify_refund,
-            SUM(CASE WHEN customer_order_index = 1 THEN subtotal_refund END) as shopify_first_refund
-        FROM refund_order_data
-        GROUP BY date_granularity, {{granularity}}
-        {% if not loop.last %}UNION ALL{% endif %}
-        {% endfor %}
-    ),
-    
+with
 paid_data as
     (SELECT channel, campaign_id::varchar as campaign_id, campaign_name, date::date, date_granularity, COALESCE(SUM(spend),0) as spend, COALESCE(SUM(clicks),0) as clicks, 
         COALESCE(SUM(impressions),0) as impressions, COALESCE(SUM(add_to_cart),0) as add_to_cart, COALESCE(SUM(paid_purchases),0) as paid_purchases, COALESCE(SUM(paid_revenue),0) as paid_revenue, 
@@ -114,17 +86,17 @@ sho_data as
 			0 as add_to_cart,
             0 as paid_purchases,
             0 as paid_revenue, 
-            COALESCE(SUM(shopify_first_orders),0) as shopify_first_orders, 
-            COALESCE(SUM(shopify_orders),0) as shopify_orders, 
-            COALESCE(SUM(shopify_first_sales),0) as shopify_first_sales, 
-            COALESCE(SUM(shopify_sales), 0) as shopify_sales,
-            COALESCE(SUM(shopify_first_sales),0)-COALESCE(SUM(shopify_first_refund),0) as shopify_first_net_sales,
-            COALESCE(SUM(shopify_sales),0)-COALESCE(SUM(shopify_refund),0) as shopify_net_sales,
+            first_orders as shopify_first_orders, 
+            orders as shopify_orders, 
+            first_order_gross_sales as shopify_first_sales, 
+            gross_sales as shopify_sales,
+            first_order_net_sales as shopify_first_net_sales,
+            net_sales as shopify_net_sales,
             0 as sessions,
             0 as engaged_sessions,
             0 as ga4_purchases,
             0 as ga4_revenue 
-        FROM initial_sho_data 
+    	FROM {{ source('reporting','shopify_sales') }}
         GROUP BY channel, date, campaign_name, date_granularity
     )
     
