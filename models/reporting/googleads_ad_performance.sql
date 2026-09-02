28e897701badc51c551097532077cc9e429d684f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-09-02 13:50:38 +0200

    Add new metrics for add_to_cart and purchases

--- models/reporting/googleads_ad_performance.sql
+++ models/reporting/googleads_ad_performance.sql
@@ -16,6 +16,7 @@ date_granularity,
 spend,
 impressions,
 clicks,
-conversions as purchases,
-conversions_value as revenue
+addtocart as add_to_cart,
+dasale+httpswwwmustelausacomga4webpurchase as purchases,
+dasale_value+httpswwwmustelausacomga4webpurchase_value as revenue
 FROM {{ ref('googleads_performance_by_ad') }}
